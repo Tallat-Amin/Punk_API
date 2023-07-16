0a5dca94311d41548f055ffa88b75f05b5b8d6fa
Tallat Amin <106815803+Tallat-Amin@users.noreply.github.com>
2023-07-17 02:24:19 +0500

    Added Search and box shadow for cards

--- src/Components/Header/Main.js
+++ src/Components/Header/Main.js
@@ -1,71 +1,67 @@
 import styles from "./main.module.scss";
 import axios from "axios";
 import { useEffect, useState } from "react";
-const inputSearch = document.querySelector("input");
-// (e.target.value = inputSearch.value ? console.log(e.target.value) : ""),
-
+const API_link = "https://api.punkapi.com/v2/beers/";
 const Main = () => {
   const [beerList, setBeerList] = useState([[]]);
-  const [inputText, setInputText] = useState("");
-  // inputSearch.addEventListener("input", (e) => {
-  //   setInputText(e.target.value);
-  // });
   const getBeerApi = async () => {
-    const resp = await axios.get(`https://api.punkapi.com/v2/beers/`);
-    setBeerList(resp.data);
-  };
-  const fetchBeerData = async () => {
-    const resp = await axios.get(
-      `https://api.punkapi.com/v2/beers?beer_name=${inputText}`,
-    );
-    setBeerList(resp.data);
+    try {
+      const resp = await axios.get(API_link);
+      setBeerList(resp.data);
+    } catch (error) {
+      console.error(`Error fetching beer data: ${error}`);
+    }
   };
   useEffect(() => {
     getBeerApi();
-  });
-  // useEffect(() => {
-  //   fetchBeerData();
-  // });
-  // const filterBeer = async () => {
-  //   const beerName =
-  //   const resp = await axios.get(
-  //     `https://api.punkapi.com/v2/beers?beer_name=${beerName}`,
-  //   );
-  // };
+  }, []);
+  const handleSearch = async (e) => {
+    // console.log(e.target.value);
+    try {
+      if (e.target.value === "") {
+        const resp = await axios.get(`${API_link}`);
+        setBeerList(resp.data);
+      } else {
+        const resp = await axios.get(`${API_link}?beer_name=${e.target.value}`);
+        setBeerList(resp.data);
+      }
+    } catch (error) {
+      console.error("Error getting beer list: " + error);
+    }
+  };
+
   return (
     <div className={styles.main}>
-      <>
-        <div className={styles.main_header}>
-          <h2>Punk API using Axios</h2>
-          <div className={styles.search}>
-            <input
-              type="text"
-              className={styles.input}
-              placeholder={"Enter beer...."}
-            />
-          </div>
+      <div className={styles.main_header}>
+        <h2>Punk API using Axios</h2>
+        <div className={styles.search}>
+          <input
+            type="text"
+            className={styles.input}
+            onChange={handleSearch}
+            placeholder={"Enter beer...."}
+          />
         </div>
-        {/*<button onClick={getBeerApi}>Press to get beer data</button>*/}
-        <div className={styles.beers}>
-          {beerList.length >= 1 ? (
-            beerList.map((beer, index) => {
-              const { id, name, image_url, tagline, first_brewed } = beer;
-              const imgName = image_url ? image_url.split("/").pop() : "";
-              // console.log(imgName);
-              return (
-                <div id={id}>
-                  <h3>{name}</h3>
-                  <img src={image_url} alt={imgName} height={100} width={45} />
-                  <h4>{tagline}</h4>
-                  <p>{first_brewed}</p>
-                </div>
-              );
-            })
-          ) : (
-            <></>
-          )}
-        </div>
-      </>
+      </div>
+      {/*<button onClick={getBeerApi}>Press to get beer data</button>*/}
+      <div className={styles.beers}>
+        {beerList.length >= 1 ? (
+          beerList.map((beer) => {
+            const { id, name, image_url, tagline, first_brewed } = beer;
+            const imgName = image_url ? image_url.split("/").pop() : "";
+            return (
+              <div id={id}>
+                <h3>{name}</h3>
+                <img src={image_url} alt={imgName} height={100} width={45} />
+                <h4>{tagline}</h4>
+                <p>{first_brewed}</p>
+              </div>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
